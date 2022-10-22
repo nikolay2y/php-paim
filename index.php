3c753ec482278eefee07dad6418109c91b5f2774
nikolay2y <chigirevnn@gmail.com>
2022-10-22 05:33:29 -0700

    add debug function

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+declare(strict_types=1);
 
-echo "Test";
+
+function dump($params)
+{
+    echo ('
+    <div style ="
+        display:inline-block;
+        background: lightgray;
+        border:1px solid gray;
+        padding:10px;
+    ">
+    <pre>
+    ');
+    print_r($params);
+    echo ('</pre></div><br>');
+}
---
+++ src/utils/debug.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace App;
+include_once('./src/utils/debug.php');
+// include
+// include_once
+// require
+// require_once
+dump('test');
+dump('==== dump ====');
